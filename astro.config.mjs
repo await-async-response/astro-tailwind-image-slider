0b720c15e235aa298088cac7f08c646c55d93a0f
Fabian <none>
2025-10-23 21:54:24 +0200

    feat: added github action for pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,8 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  
+  site: 'https://await-async-response.github.io/astro-tailwind-image-slider/',
+  outDir: './dist',
 });
